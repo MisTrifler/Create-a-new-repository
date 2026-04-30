5a88d7fd2554fe438aa4c9c17756f25eb0ef3b39
MisTrifler <vidyutc900@gmail.com>
2026-05-01 00:12:07 +0100

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Your Dashboard</h1>
+      <p>Your saved deals and posted deals will appear here.</p>
+    </div>
+  );
+}
